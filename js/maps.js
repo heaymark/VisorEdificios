256b9286fc622bbca4d6019c0b301f1b6b765f7c
Mario Rivera <maro.arivera@gmail.com>
2017-09-27 15:28:02 -0500

    Mod select layer checkbox
    Modificacion del select layer con checkbox

--- js/maps.js
+++ js/maps.js
@@ -24,24 +24,61 @@ var param = {
 };
 var viz = "https://finanzasdf.carto.com/u/develop/api/v2/viz/d2dc6eb1-1dd4-4eb8-8aa9-d629bba144d9/viz.json";
 
-$(function(){
+$(document).ready(function(){
 
-	objlayerBase = L.tileLayer(layerbase,paramlayerbase);
-	maps = new L.Map(mapdiv, parammapbase), maps.addLayer(objlayerBase); //aqui se genera el mapa base
-	cartodb.createLayer(maps,viz) //aqui se jala el mapa que esta en el visor, objeto del mapa y variable viz
-		.addTo(maps)
-		.on('done', function(layer){//evento que se dispara cuando kla peticion a carto
-			lyrs = layer;
-            // When the layers inputs change fire this
-            $("input[name='layer']").change(function(){
-				// Clear the sublayers
-				layer.getSubLayers().forEach(function(sublayer){sublayer.remove()});
-				// For every check activated, add a sublayer
-				$.each($("input[name='layer']:checked"), function(){
-					layer.createSubLayer(jsonlayers[$(this).attr("id")]);
-              	});
-            });
+	// objlayerBase = L.tileLayer(layerbase,paramlayerbase);
+	// maps = new L.Map(mapdiv, parammapbase), maps.addLayer(objlayerBase); //aqui se genera el mapa base
+	// cartodb.createLayer(maps,viz) //aqui se jala el mapa que esta en el visor, objeto del mapa y variable viz
+        var maps = new L.Map('maps', {
+            zoomControl: true,
+            center: new L.LatLng(19.33123050921937,-99.09942626953125),
+            zoom : 10,
+            minZoom: 0,
+            maxZoom: 20,
+            attribution: "CARTO",
+        });
+        var basemap = L.tileLayer(layerbase,paramlayerbase).addTo(maps);
+	
+    // 	.on('done', function(layer){//evento que se dispara cuando kla peticion a carto
+	// 		lyrs = layer;
+    //      
+    // Layers definition
+    
+        var layers = {
+            'cair': {
+                sql: 'SELECT * FROM cair',
+                // cartocss: '#layer{polygon-fill: #D6301D;polygon-opacity: 0.7;}'
+            },
+            'edificios_en_peligro1': {
+                sql: 'SELECT * FROM edificios_en_peligro1',
+                // cartocss: '#rios_btn25{line-color: #2E5387;line-width: 2;line-opacity: 0.7;}'
+            },
+            'delegaciones_df_1': {
+                sql: 'SELECT * FROM delegaciones_df_1',
+                // cartocss: '#aloj_ocio_btn100{marker-fill-opacity: 0.9; marker-line-color: #FFF; marker-line-width: 1.5; marker-width: 10; marker-fill: #3B007F; }'
+            }
+        }
 
+
+            cartodb.createLayer(maps,{
+                user_name: 'develop',
+                type: 'cartodb',
+                sublayers: []
+              })
+            .addTo(maps)
+            // .on('done', function(layer){
+                .done(function(layer){
+                // lyrs = layer;
+                // alert($('#delegaciones_df_1:checked').val());
+                // When the layers inputs change fire this
+                    $("input[name='layer']").change(function(){
+                        // Clear the sublayers
+                        layer.getSubLayers().forEach(function(sublayer){sublayer.remove()});
+                        // For every check activated, add a sublayer
+                        $.each($("input[name='layer']:checked"), function(){
+                            layer.createSubLayer(layers[$(this).attr("id")]);
+                        });
+                    });
 
 		}).error(function (err) {
 			console.log(err);
@@ -75,8 +112,8 @@ $(function(){
 					    	}
 					    }
 					    // var layers2 = layers+layers;
-					    console.log(jsonlayers);
-						select_layerts(data.rows[idx].name, data.rows[idx].layers, jsonlayers);
+					    // console.log(jsonlayers);
+						select_layerts(data.rows[idx].name, data.rows[idx].layer, jsonlayers);
 
 	            	}//Fin for(idx in data.rows){
 
@@ -84,42 +121,72 @@ $(function(){
 
 });
 
-function select_layerts(name,layers,jsonlayers){
-
-	// $('<li>', { 'class': 'layer'}).appendTo('#list-layer').fadeIn('slow');
-	// $('<input />', { type: 'checkbox', id: name, value: name }).appendTo('#list-layer').fadeIn('slow');
-	// $('<label />', { 'for': 'cb', text: name }).appendTo('#list-layer').fadeIn('slow');
-
+function select_layerts(nombrecapa,capa,jsonlayers){
+console.log(capa);
 	$('<li>', {
 		'class': 'layer'
 	    }).append(
 	    	$('<input/>',{
-	      		type: 'checkbox', 
-	      		id: layers, 
+	      		type: 'checkbox',
+                data_layer: nombrecapa,
+	      		id: capa, 
 	      		name: 'layer',
 	      	}),
 		    $('<label>',{
-		    	text:name
+		    	text:nombrecapa
 		    })
 	).hide().appendTo('#list-layer').fadeIn('slow');	
 
-        cartodb.createLayer(maps,{
-            user_name: 'develop',
-            type: 'cartodb',
-            sublayers: []
-          })
-          .addTo(maps)
-          .done(function(layer){
-            // When the layers inputs change fire this
-            $("input[name='layer']").change(function(){
-              // Clear the sublayers
-              layer.getSubLayers().forEach(function(sublayer){sublayer.remove()});
-              // For every check activated, add a sublayer
-              $.each($("input[name='layer']:checked"), function(){
-                  layer.createSubLayer(jsonlayers[$(this).attr("id")]);
-              });
-            });
-        });
+                alert($('#delegaciones_df_1:checked').val());
+
+
+}
+
+function layers(capa){
+    switch(capa){
+        case 'PEA':
+            idxvisible = 3;
+            // lyrs.getSubLayer(3).toggle();
+            if(map.hasLayer(sltfeature)){
+                map.removeLayer(sltfeature);
+            };
+        break;
+        case 'PHM18':
+            idxvisible = 2;
+            // lyrs.getSubLayer(2).toggle();
+
+            if(map.hasLayer(sltfeature)){
+                map.removeLayer(sltfeature);
+            };
+        break;
+        case 'PMM18':
+            idxvisible = 1;
+            // lyrs.getSubLayer(1).toggle();
+
+            if(map.hasLayer(sltfeature)){
+                map.removeLayer(sltfeature);
+            };
+        break;
+        case 'PT':
+            //obtiene una subcapa creada (getSubLayer)
+            //alterna la vista de la subcapa devuelte boolean (toggle)
+                
+            if(map.hasLayer(sltfeature)){
+                map.removeLayer(sltfeature);
+            };
+
+        break;
+    }
+
+    var idx = 0;
+    for(idx=0;idx<=7;idx++){
+        if (idxvisible == idx){
+            lyrs.getSubLayer(idx).toggle();
+        } else {
+            lyrs.getSubLayer(idx).hide();
+        }   
+
+    }
 }
 
 //Funcion para acercar al mapa 
