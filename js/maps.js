443ee54ca786afe62d1ccf0a70c2f8cdf5c63790
Mario Rivera <maro.arivera@gmail.com>
2017-09-28 10:03:51 -0500

    Second version
    second changes

--- js/maps.js
+++ js/maps.js
@@ -26,66 +26,44 @@ var viz = "https://finanzasdf.carto.com/u/develop/api/v2/viz/d2dc6eb1-1dd4-4eb8-
 
 $(document).ready(function(){
 
-	// objlayerBase = L.tileLayer(layerbase,paramlayerbase);
-	// maps = new L.Map(mapdiv, parammapbase), maps.addLayer(objlayerBase); //aqui se genera el mapa base
-	// cartodb.createLayer(maps,viz) //aqui se jala el mapa que esta en el visor, objeto del mapa y variable viz
-        var maps = new L.Map('maps', {
-            zoomControl: true,
-            center: new L.LatLng(19.33123050921937,-99.09942626953125),
-            zoom : 10,
-            minZoom: 0,
-            maxZoom: 20,
-            attribution: "CARTO",
-        });
-        var basemap = L.tileLayer(layerbase,paramlayerbase).addTo(maps);
-	
-    // 	.on('done', function(layer){//evento que se dispara cuando kla peticion a carto
-	// 		lyrs = layer;
-    //      
-    // Layers definition
-    
-        var layers = {
-            'cair': {
-                sql: 'SELECT * FROM cair',
-                // cartocss: '#layer{polygon-fill: #D6301D;polygon-opacity: 0.7;}'
-            },
-            'edificios_en_peligro1': {
-                sql: 'SELECT * FROM edificios_en_peligro1',
-                // cartocss: '#rios_btn25{line-color: #2E5387;line-width: 2;line-opacity: 0.7;}'
-            },
-            'delegaciones_df_1': {
-                sql: 'SELECT * FROM delegaciones_df_1',
-                // cartocss: '#aloj_ocio_btn100{marker-fill-opacity: 0.9; marker-line-color: #FFF; marker-line-width: 1.5; marker-width: 10; marker-fill: #3B007F; }'
-            }
+    objlayerBase = L.tileLayer(layerbase,paramlayerbase);
+    maps = new L.Map(mapdiv, parammapbase), maps.addLayer(objlayerBase); //aqui se genera el mapa base
+    cartodb.createLayer(maps,viz) //aqui se jala el mapa que esta en el visor, objeto del mapa y variable viz
+        .addTo(maps)
+        .on('done', function(layer) {
+
+        lyrs = layer;
+        // console.log(layer.layers);
+        // console.log(layer.layers[0].options.layer_name);//nombre de la capas
+        // console.log(layer.layers.length);//cantidad de capas
+        for (var i = 0; i < layer.layers.length; i++) {
+            console.log("Num capa " +i+" "+ layer.layers[i].options.layer_name);//nombre de la capas
         }
 
+        objsql = new cartodb.SQL({user:"develop",});//Fin objsql = new cartodb.SQL({
+        objsql.execute("SELECT name, layer FROM select_layer_copy")
+            .on("done",function(data){
+                    for(idx in data.rows){
+                        var name = data.rows[idx].name;
+                        var name2 =  String(name);
 
-            cartodb.createLayer(maps,{
-                user_name: 'develop',
-                type: 'cartodb',
-                sublayers: []
-              })
-            .addTo(maps)
-            // .on('done', function(layer){
-                .done(function(layer){
-                // lyrs = layer;
-                // alert($('#delegaciones_df_1:checked').val());
-                // When the layers inputs change fire this
-                    $("input[name='layer']").change(function(){
-                        // Clear the sublayers
-                        layer.getSubLayers().forEach(function(sublayer){sublayer.remove()});
-                        // For every check activated, add a sublayer
-                        $.each($("input[name='layer']:checked"), function(){
-                            layer.createSubLayer(layers[$(this).attr("id")]);
-                        });
-                    });
+                        // Layers definition
+                        var jsonlayers = {
+                            'nombre': {
+                                sql: 'SELECT * FROM '+data.rows[idx].layer,
+                                cartocss: "#countries['mapnik::geometry_type'=3] { polygon-fill: #fff; } 2 line 1  punto"
+                            }
+                        }
+                        // var layers2 = layers+layers;
+                        // console.log(jsonlayers);
+                        select_layerts(data.rows[idx].name, data.rows[idx].layer, jsonlayers);
 
-		}).error(function (err) {
-			console.log(err);
+                    }//Fin for(idx in data.rows){
+            });//Fin .on("done",function(data){
+
+        }).error(function (err) {
+		 	console.log(err);
 		});
-
-    	// maps.on("zoomlevelschange",mapGrafica);
-    	// maps.on("moveend",mapGrafica);
 
     	maps.on("zoomlevelschange",mapGraficalinea);
     	maps.on("moveend",mapGraficalinea);  
@@ -95,29 +73,6 @@ $(document).ready(function(){
     	  	
 		maps.on("zoomlevelschange",mapGraficacolum2);
     	maps.on("moveend",mapGraficacolum2);  
-
-		objsql = new cartodb.SQL({user:"develop",});//Fin objsql = new cartodb.SQL({
-		objsql.execute("SELECT name, layer FROM select_layer_copy")
-	
-	        .on("done",function(data){
-	        	   	for(idx in data.rows){
-	        	   		var name = data.rows[idx].name;
-	        	   		var name2 =  String(name);
-							
-					    // Layers definition
-					    var jsonlayers = {
-					    	'nombre': {
-					        	sql: 'SELECT * FROM '+data.rows[idx].layer,
-            					cartocss: "#countries['mapnik::geometry_type'=3] { polygon-fill: #fff; } 2 line 1  punto"
-					    	}
-					    }
-					    // var layers2 = layers+layers;
-					    // console.log(jsonlayers);
-						select_layerts(data.rows[idx].name, data.rows[idx].layer, jsonlayers);
-
-	            	}//Fin for(idx in data.rows){
-
-	    	});//Fin .on("done",function(data){
 
 });
 
@@ -137,9 +92,11 @@ console.log(capa);
 		    })
 	).hide().appendTo('#list-layer').fadeIn('slow');	
 
-                alert($('#delegaciones_df_1:checked').val());
+    // alert($('#delegaciones_df_1:checked').val());
 
-
+    if ($('#'+capa).is(':checked') ) {
+        console.log("Checkbox seleccionado"+capa);
+    }
 }
 
 function layers(capa){
